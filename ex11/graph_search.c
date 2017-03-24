5a0f60db055210b9070f779badd446f5cce76248
TuringFallSleep <boyin.yang@student.manchester.ac.uk>
2017-03-24 17:06:17 +0000

    finish ex11

--- ex11/graph_search.c
+++ ex11/graph_search.c
@@ -37,6 +37,7 @@ int dequeue(int* queue,int MaxSize){
 
 void BFS (Graph* mygraph, int sNode, int lNode)
 {
+	printf("\nBFS:\n");
 	int queue[mygraph->MaxSize];//(List*)malloc(sizeof(List)*mygraph->MaxSize*mygraph->MaxSize);
 	int set[mygraph->MaxSize];
 	int root = sNode;
@@ -56,17 +57,19 @@ void BFS (Graph* mygraph, int sNode, int lNode)
 	{
 		int current = dequeue(queue,mygraph->MaxSize);
 		
-		if (mygraph->table[current].outdegree == mygraph->table[lNode].outdegree)
-		{
-			printf("Node %d with smallest(non-zero) out-degree can reach the node %d with largest out-degree: %d.\n",
-	 		 sNode,current,mygraph->table[current].outdegree);
-			return;
-		}
-		else if (mygraph->table[current].outdegree > resultOutdegree)
+		// if (mygraph->table[current].outdegree == mygraph->table[lNode].outdegree)
+		// {
+		// 	printf("Node %d with smallest(non-zero) out-degree can reach the node %d with largest out-degree: %d.\n",
+	 // 		 sNode,current,mygraph->table[current].outdegree);
+		// 	return;
+		// }
+		// else 
+			if (mygraph->table[current].outdegree > resultOutdegree)
 		{
 			resultOutdegree = mygraph->table[current].outdegree;
 			resultIndex = current;
 		}
+
 		List *list = mygraph->table[current].outlist;
 		for (int i = 1; i < mygraph->MaxSize; i++)
 		{
@@ -76,9 +79,9 @@ void BFS (Graph* mygraph, int sNode, int lNode)
 				i++;
 				list = list->next;
 			}
-			adjacent[current][i] = 0;
-			
+			adjacent[current][i] = 0;			
 		}
+
 		int j = 1;
 		while(adjacent[current][j] != 0)
 		{
@@ -107,14 +110,95 @@ void BFS (Graph* mygraph, int sNode, int lNode)
 			j++;
 		}
 	}
-	printf("Node %d with smallest(non-zero) out-degree can reach the node %d with largest out-degree: %d.\n",
+	printf("Node %d with smallest(non-zero) out-degree can reach the node %d with largest out-degree: %d.\n\n\n\n",
 	 		 sNode,resultIndex,mygraph->table[resultIndex].outdegree);
 }
 
 
-
-
-void DFS (Graph* mygraph, int sNode, int lNode)
+void push(int* stack, int node, int MaxSize)
 {
+	for (int i = 1; i < MaxSize; i++)
+	{
+		if (stack[i] == 0)
+		{
+			stack[i] = node;
+			// printf("push node %d\n", node);
+			break;
+		}
+	}
+}
+
+int pop(int* stack, int MaxSize)
+{
+	int current;
+	if (stack[1])
+		for (int i = 2; i < MaxSize; i++)
+		{
+			if (!stack[i])
+			{
+				current = stack[i-1];
+				stack[i-1] = 0;
+				printf("pop node %d\n", current);
+				return current;
+			}
+		}
+	else
+		printf("stack[1] is %d!\n", stack[1]);
+	return 0;
+}
+
+
+void DFS (Graph* mygraph, int sNode, int lNode)// initial v = 1
+{
+	printf("DFS:\n");
+	int stack[mygraph->MaxSize];
+	int discovered[mygraph->MaxSize];
+	int current;
+	int resultIndex = 1;
+	int resultOutdegree = 0;
+	for (int i = 1; i < mygraph->MaxSize; i++)
+	{
+		stack[i] = 0;
+		discovered[i] = 0;
+	}
+	push(stack, sNode, mygraph->MaxSize);
+	while(stack[1])// stack[1] == 0 -> empty
+	{
+		current = pop(stack, mygraph->MaxSize);
+		if (!current)
+			break;
+		// if (mygraph->table[current].outdegree == mygraph->table[lNode].outdegree)
+		// {
+		// 	printf("Node %d with smallest(non-zero) out-degree can reach the node %d with largest out-degree: %d.\n",
+	 // 		 sNode,current,mygraph->table[current].outdegree);
+		// 	return;
+		// }
+		// else 
+			if (mygraph->table[current].outdegree > resultOutdegree)
+		{
+			resultOutdegree = mygraph->table[current].outdegree;
+			resultIndex = current;
+		}
+
+		if (!discovered[current])
+		{
+			discovered[current] = 1;
+			List *list = mygraph->table[current].outlist;
+			
+			while(list)
+			{
+				// printf("in loop 2\n");
+				if (!discovered[list->index])
+				{
+					push(stack, list->index, mygraph->MaxSize);
+				}
+				
+				list = list->next;
+			}		
+			
+		}
+	}
+	printf("Node %d with smallest(non-zero) out-degree can reach the node %d with largest out-degree: %d.\n",
+	 		 sNode,resultIndex,mygraph->table[resultIndex].outdegree);
 	return;
 }
--- ex11/part2.c
+++ ex11/part2.c
@@ -30,7 +30,7 @@ int main(int argc,char *argv[])
   Graph mygraph;
 
   read_graph(&mygraph,argv[1]);
-  print_graph(&mygraph);
+  // print_graph(&mygraph);
   	
 
   /* you take it from here */
